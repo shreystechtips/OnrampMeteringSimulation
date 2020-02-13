bdeac9652af4e63fadead508e5f71447d483bdd2
matt-ketk <mketkaroonkul@gmail.com>
2020-02-13 14:58:00 -0800

    some more pg implementation and roads

---
+++ road.py
@@ -0,0 +1,15 @@
+import numpy as np
+
+
+
+class Road:
+    def __init__(self, right_lanes=1, left_lanes=1):
+        # CONSTANTS        
+        self.LANE_WIDTH = 50
+        self.DRIVES_LEFT = False
+        self.right_lanes = right_lanes
+        self.left_lanes = left_lanes
+    
+        # self.numpy_form = 
+
+    
