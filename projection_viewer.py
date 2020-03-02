6ba9098edec5ea7ecc21dc3ceebfaf0276f3ed58
matt-ketk <mketkaroonkul@gmail.com>
2020-03-02 14:48:37 -0800

    rotations is broken

--- projection_viewer.py
+++ projection_viewer.py
@@ -2,6 +2,7 @@
 import pygame as pg
 import numpy as np
 import main as rects
+import constants as const
 # modules
 
 def rot_matrix(angle):
@@ -19,8 +20,13 @@ class ProjectionViewer:
         self.screen = pg.display.set_mode((width, height))
         pg.display.set_caption('Traffic Simulation v1')
         self.background = (10, 10, 50)
+<<<<<<< HEAD
         self.cars = rects.create_rects(250, size_x=10, size_y=10, lanes = range(50,800, 50))
         self.roads = []
+=======
+        self.cars_x,self.cars_y,self.velocity_x,self.velocity_y = rects.create_rects(50, size_x=const.CONST_CAR_SIZE_X, size_y=const.CONST_CAR_SIZE_Y, lanes = range(50,800, 50))
+
+>>>>>>> 1a52e39ad19ea9828656ced02b95e5ce502135c9
 
         self.iterator = 0
         self.edge_color = (200,200,200)
@@ -36,7 +42,7 @@ class ProjectionViewer:
             self.screen.fill(self.background)
             self.display()
             pg.display.flip()
-            pg.time.delay(50)
+            pg.time.delay(const.CONST_FRAME_TIME_MS)
     
  
     def display(self):
@@ -66,6 +72,12 @@ class ProjectionViewer:
             [length, radius],
             [length, -radius]
         ])
+        # self.iterator += 1
+        # self.cars_x,self.cars_y,self.velocity_x,self.velocity_y = rects.update_rects(self.cars_x,self.cars_y,self.velocity_x,self.velocity_y, self.iterator)
+        # iter = 0
+        # while iter < len(self.cars_x):
+            # pg.draw.rect(self.screen, (50, 40, 30), pg.Rect(self.cars_x[iter][0],self.cars_y[iter][0],const.CONST_CAR_SIZE_X,const.CONST_CAR_SIZE_Y))
+            # iter += 1
 
         graphic_points = (pre_rotated - road.start) @ rot_matrix(angle) + road.start 
         # print(graphic_points[0])
